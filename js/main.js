b46688a96835cafb12cbc936fc8a4f1c31fc8ed7
kosta891 <155198914mk>
2019-12-10 21:02:11 +0100

    destkop verzija

--- js/main.js
+++ js/main.js
@@ -1,77 +1,65 @@
+
 (function(){
-const form = document.getElementsByClassName('search-form')[0];
+                
+        const form = document.getElementsByClassName('search-form')[0];
+        let inputField = document.getElementsByClassName('search-input')[0]
+        // vrednost inputa u console log
+        form.addEventListener('submit', function (e) {
+                e.preventDefault()
+                let input = inputField.value;
+                if(input === ''){
+                        alert('Please fill input field properly')
+                }
+                console.log(input);
+                inputField.value = ''
+                inputField.focus();
+                
+        })
 
-let inputField = document.getElementsByClassName('search-input')[0]
 
-form.addEventListener('submit', function (e) {
-        e.preventDefault()
-        let input = inputField.value;
-        inputField.value = ''
-        inputField.focus();
+        const imageWrapper = document.getElementsByClassName('image-wrapper')[1];
+        const pTag = imageWrapper.getElementsByTagName( 'p');
+        const container = document.getElementsByClassName('flex-4')[0]
+
         
-})
+        
+        container.addEventListener('click' , function (e) {
+        
+                let clickedValue = e.target
+                clickedValue = clickedValue.textContent.toLowerCase()
+                console.log(clickedValue);
 
+                let textP;
 
+                for(let i = 0; i< pTag.length; i++) {
+                        textP = pTag[i]
+                        let textContentP = textP.textContent.toLowerCase();
 
-
-
-// 
-
-
-
-const imageWrapper = document.getElementsByClassName('image-wrapper')[1];
-const img = imageWrapper.getElementsByTagName('img')[0]
-const mapLocation = imageWrapper.getElementsByClassName('map-location ')
-const pTag = imageWrapper.getElementsByTagName( 'p');
-console.log(img);
-
-
-const container = document.getElementsByClassName('flex-4')[0]
-
-let locationText = container.getElementsByClassName('location-text')
-
-// kako da ubacim for petrlju za sve hover textove
-
-
-container.addEventListener('click' , function (e) {
-       
-        let clickedValue = e.target
-        clickedValue = clickedValue.textContent.toLowerCase()
-        console.log(clickedValue);
-
-        let textP;
-
-        for(let i = 0; i< pTag.length; i++) {
-                textP = pTag[i]
-                let textContentP = textP.textContent.toLowerCase();
-
-               if(clickedValue === textContentP ) { //videti za input da se prikaze
-                       
-                       textP.classList.add('active');
-                       console.log();
+                if(clickedValue === textContentP ) { 
+                        
+                        textP.classList.add('active');
+                        
                 } else{
                         textP.classList.remove('active')
                 }
-              
-                removingClassOfP(textP)
                 
-        } 
+                removingClassOfP(textP)
+                        
+                } 
 
-       
         
-})
-
-function removingClassOfP(pClass) { 
-
-        imageWrapper.addEventListener('click', function () {
-                pClass.classList.remove('active')
+                
         })
-}
+
+        function removingClassOfP(pClass) { 
+
+                imageWrapper.addEventListener('click', function () {
+                        pClass.classList.remove('active')
+                })
+        }
 
 
 
 })()
       
-
-  
 
